fdba9580a7e9c0eac3770bed1a1a264ba11030f0
Parham Alvani <parham.alvani@gmail.com>
2021-04-22 18:55:31 +0430

    feat: Update Arch Image Downloader

--- arch-latest.sh
+++ arch-latest.sh
@@ -9,4 +9,4 @@
 # =======================================
 
 most_recent=$(curl -Ls 'https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/artifacts/master/browse/output?job=build:secure' | grep cloudimg | grep -vi sha256 | sed "s/.* href=\"\(.*\)\".*/\1/" | tail -1 | sed "s|artifacts/file|artifacts/raw|")
-curl -LO  "https://gitlab.archlinux.org$most_recent"{,.SHA256}
+curl -LO "https://gitlab.archlinux.org$most_recent"{,.SHA256}
