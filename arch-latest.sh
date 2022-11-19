8368ff61803d710c8e854a8546ba0d5504cd0845
Parham Alvani <parham.alvani@gmail.com>
2022-11-19 23:32:52 +0330

    chore: remove manual scripts forever, we have vagrant

--- arch-latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "dowload latest release of archlinux from gitlab.archlinux.org"
-
-most_recent=$(curl -Ls 'https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/artifacts/master/browse/output?job=build:secure' |
-	grep cloudimg | grep -vi sha256 | grep -vi sig | sed "s/.* href=\"\(.*\)\".*/\1/" | tail -1 | sed "s|artifacts/file|artifacts/raw|")
-curl -LO "https://gitlab.archlinux.org$most_recent"{,.SHA256}
